bc40af354d464b45739f6ea453bf13338760695c
Asad-Ismail <asadismaeel@gmail.com>
2023-04-20 13:32:03 +0200

    updated onnx infrence

--- backend/run_onnx_inference.py
+++ backend/run_onnx_inference.py
@@ -33,8 +33,27 @@ ort_session = onnxruntime.InferenceSession(onnx_model_path)
 sam.to(device='cuda')
 predictor = SamPredictor(sam)
 
+image_embedding=None
 
 def set_image(image):
+    global image_embedding
     print(f"Setting Image in model!")
     predictor.set_image(image)
+    image_embedding = predictor.get_image_embedding().cpu().numpy()
 
+def get_mask(image,point,label):
+    global image_embedding
+    onnx_coord = np.concatenate([point, np.array([[0.0, 0.0]])], axis=0)[None, :, :]
+    onnx_label = np.concatenate([label, np.array([-1])], axis=0)[None, :].astype(np.float32)
+    onnx_coord = predictor.transform.apply_coords(onnx_coord, image.shape[:2]).astype(np.float32)
+    onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
+    onnx_has_mask_input = np.zeros(1, dtype=np.float32)
+    ort_inputs = {
+    "image_embeddings": image_embedding,
+    "point_coords": onnx_coord,
+    "point_labels": onnx_label,
+    "mask_input": onnx_mask_input,
+    "has_mask_input": onnx_has_mask_input,
+    "orig_im_size": np.array(image.shape[:2], dtype=np.float32)
+    }
+    masks, probs, low_res_logits = ort_session.run(None, ort_inputs)
