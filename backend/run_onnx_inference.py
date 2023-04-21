b964bd0b3d42020aec642756700348b2bd4522f4
Asad-Ismail <asadismaeel@gmail.com>
2023-04-21 18:17:33 +0200

    disabled low res masks need further checks

--- backend/run_onnx_inference.py
+++ backend/run_onnx_inference.py
@@ -58,13 +58,13 @@ def get_mask(image,point,label):
     onnx_coord = np.concatenate([point, np.array([[0.0, 0.0]])], axis=0)[None, :, :]
     onnx_label = np.concatenate([label, np.array([-1])], axis=0)[None, :].astype(np.float32)
     onnx_coord = predictor.transform.apply_coords(onnx_coord, image.shape[:2]).astype(np.float32)
-    if low_res_masks:
-        print(f"Using previous low res masks")
-        onnx_mask_input = low_res_masks
-        onnx_has_mask_input = np.ones(1, dtype=np.float32)
-    else:
-        onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
-        onnx_has_mask_input = np.zeros(1, dtype=np.float32)
+    #if low_res_masks is not None:
+    #    print(f"Using previous low res masks")
+    #    onnx_mask_input = low_res_masks
+    #    onnx_has_mask_input = np.ones(1, dtype=np.float32)
+    #else:
+    onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
+    onnx_has_mask_input = np.zeros(1, dtype=np.float32)
     ort_inputs = {
     "image_embeddings": image_embedding,
     "point_coords": onnx_coord,
